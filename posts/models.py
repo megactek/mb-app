3ce181d55819177dbb0c81291650bbbaef92d88b
t1nidog <mgcrons@gmail.com>
2021-06-12 14:49:14 +0100

    initial commit

---
+++ posts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.db.models.base import Model
+
+# Create your models here.
+class Post(models.Model):
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text[:50]
